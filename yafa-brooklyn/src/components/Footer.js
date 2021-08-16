578ada6ab876adea3b1a39311f0a804b41fad57b
Halgamoos <halgamoos@gmail.com>
2021-08-16 01:46:11 -0400

    updated Footer

--- yafa-brooklyn/src/components/Footer.js
+++ yafa-brooklyn/src/components/Footer.js
@@ -1,101 +1,19 @@
 import React from 'react';
 import './Footer.css';
-import { Button } from './Button';
-import { Link } from 'react-router-dom';
+import seamless_icon from '../images/seamless.png';
 
 function Footer() {
+
   return (
     <div className='footer-container'>
-      {/* <section className='footer-subscription'>
+      <section className='footer-subscription'>
         <p className='footer-subscription-heading'>
-          Join the Adventure newsletter to receive our best vacation deals
+          Order on UberEats or Seamless and follow the socials!
         </p>
-        <p className='footer-subscription-text'>
-          You can unsubscribe at any time.
-        </p>
-        <div className='input-areas'>
-          <form>
-            <input
-              className='footer-input'
-              name='email'
-              type='email'
-              placeholder='Your Email'
-            />
-            <Button buttonStyle='btn--outline'>Subscribe</Button>
-          </form>
-        </div>
-      </section> */}
-      <div class='footer-links'>
-        <div className='footer-link-wrapper'>
-          <div class='footer-link-items'>
-            <h2>About Us</h2>
-            {/* <Link to='/'>Terms of Service</Link> */}
-          </div>
-          <div class='footer-link-items'>
-            <h2>Contact Us</h2>
-          </div>
-        </div>
-        <div className='footer-link-wrapper'>
-          <div class='footer-link-items'>
-            <h2>Videos</h2>
-          </div>
-          <div class='footer-link-items'>
-            <h2>Social Media</h2>
-          </div>
-        </div>
-      </div>
-      <section class='social-media'>
-        <div class='social-media-wrap'>
-          <div class='footer-logo'>
-            <Link to='/' className='social-logo'>
-              TRVL
-              <i class='fab fa-typo3' />
-            </Link>
-          </div>
-          <small class='website-rights'>TRVL © 2020</small>
-          <div class='social-icons'>
-            <Link
-              class='social-icon-link facebook'
-              to='/'
-              target='_blank'
-              aria-label='Facebook'
-            >
-              <i class='fab fa-facebook-f' />
-            </Link>
-            <Link
-              class='social-icon-link instagram'
-              to='/'
-              target='_blank'
-              aria-label='Instagram'
-            >
-              <i class='fab fa-instagram' />
-            </Link>
-            <Link
-              class='social-icon-link youtube'
-              to='/'
-              target='_blank'
-              aria-label='Youtube'
-            >
-              <i class='fab fa-youtube' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
-            >
-              <i class='fab fa-twitter' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='LinkedIn'
-            >
-              <i class='fab fa-linkedin' />
-            </Link>
-          </div>
-        </div>
+        <button href = "https://www.seamless.com/menu/yafa-cafe-4415-4th-ave-brooklyn/2043300">
+          <img src={seamless_icon} style = {{height: 30, width: 80}} alt="seamless" />
+        </button>
+        
       </section>
     </div>
   );
